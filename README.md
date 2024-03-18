994d263b90a82a5b5941d44c4ae00888c9ae5d5f
Sarujan P <saru.contacts@gmail.com>
2024-03-18 14:12:15 +0530

    react-dashboard initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Author: Sarujan P
+## Title: React Dashboard
+## Mobile: +94775428041
+## Email: saru.contacts@gmail.com
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-600">
+      Hello World!
+    </div>
+  )
+}
+
+export default App
